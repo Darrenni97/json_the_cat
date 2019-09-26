4494dede19ec40ac924e36393d3e478d46f2062a
Darrenni97 <Darrenni_@hotmail.com>
2019-09-26 21:17:18 +0000

    fixed lint

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,17 +4,16 @@ const args = process.argv.slice(2);
 const breed = args[0];
 
 
-request(`https://aspi.thecatapi.com/v1/breeds/search?q=${breed}`, function (error, response, body) {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
 
   if (error) {
-    console.log('Error')
+    console.log(error);
     throw Error;
   } else if (response['body'] === '[]') {
-    console.log('Requested breed does not exist.')
+    console.log('Requested breed does not exist.');
   } else if (response['body'] !== '[]') {
     const data = JSON.parse(body);
     console.log(data[0]['description']);
   }
-
 
 });

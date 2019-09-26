dd08548982793713b6b89c2a1578acb64055ceba
Darrenni97 <Darrenni_@hotmail.com>
2019-09-26 15:12:18 -0700

    refectored to run with callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,20 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-const breed = args[0];
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  
+    if (error) {
+      callback(error, null);
+    } else if (body === '[]') {
+      callback('Requested breed does not exist.', null);
+    } else if (body !== '[]') {
+      const data = JSON.parse(body);
+      callback(null, data[0]['description']);
+    }
+
+  });
+};
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-
-  if (error) {
-    console.log(error);
-    throw Error;
-  } else if (response['body'] === '[]') {
-    console.log('Requested breed does not exist.');
-  } else if (response['body'] !== '[]') {
-    const data = JSON.parse(body);
-    console.log(data[0]['description']);
-  }
-
-});
+module.exports = { fetchBreedDescription };

63de4d9959fe2126c013c4d2a0e883819abcfe2a
Darrenni97 <Darrenni_@hotmail.com>
2019-09-26 21:13:46 +0000

    initial commit

---
+++ breedFetcher.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const args = process.argv.slice(2);
+const breed = args[0];
+
+
+request(`https://aspi.thecatapi.com/v1/breeds/search?q=${breed}`, function (error, response, body) {
+
+  if (error) {
+    console.log('Error')
+    throw Error;
+  } else if (response['body'] === '[]') {
+    console.log('Requested breed does not exist.')
+  } else if (response['body'] !== '[]') {
+    const data = JSON.parse(body);
+    console.log(data[0]['description']);
+  }
+
+
+});
